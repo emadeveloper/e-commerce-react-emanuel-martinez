d932977124d71c613b760a927d6377fe9555bfe1
Emanuel Martinez <yinproducciones@gmail.com>
2022-08-10 22:04:51 -0300

    primer desafio react-js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const persona = {nombre: "ema", profesion: "estudiante"};
+  
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola {persona.nombre} {persona.profesion} de React
         </p>
+        <ButtonComponent />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,5 +25,10 @@ function App() {
     </div>
   );
 }
+
+const ButtonComponent = () => {
+  return <button>Primer boton React :)</button>
+}
+
 
 export default App;
